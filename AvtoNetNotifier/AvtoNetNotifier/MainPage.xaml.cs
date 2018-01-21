4b9b069550b29a5472cef4b5a10d85026df37484
Unknown <uros.ceh1@gmail.com>
2018-01-21 12:48:40 +0100

    Get page HTML source

---
+++ AvtoNetNotifier/AvtoNetNotifier/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AvtoNetNotifier
+{
+	public partial class MainPage : ContentPage
+	{
+        public const string SOURCE_URL = "https://www.avto.net/Ads/search.asp?SID=10000";
+
+        private WebParser parser;
+
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await ParseAvtoNetSource();
+        }
+
+        public async Task ParseAvtoNetSource()
+        {
+            parser = new WebParser();
+            bool status = await parser.LoadSourceAsync(SOURCE_URL);
+            if (status)
+            {
+                parser.Parse();
+            }
+        }
+	}
+}
---
+++ AvtoNetNotifier/AvtoNetNotifier/WebParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace AvtoNetNotifier
+{
+    class WebParser
+    {
+        HtmlDocument document;
+
+        public async Task<bool> LoadSourceAsync(string URL)
+        {
+            var webHTML = new HtmlWeb();
+            document = await webHTML.LoadFromWebAsync(URL);
+
+            return !String.IsNullOrEmpty(document.ParsedText);
+        }
+
+        public void Parse()
+        {
+        }
+    }
+}
